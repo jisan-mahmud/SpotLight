528a0fe31618b6db9c24e59a099e5b356f000192
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-03-12 15:37:42 +0600

    react native setup

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,39 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+import { COLORS } from '../../constants/theme'
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{
+        tabBarShowLabel: false, 
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.grey,
+        tabBarStyle: {
+          backgroundColor: 'black',
+          elevation: 0,
+          height: 40,
+          paddingBottom: 8
+
+        },
+      }}>
+        <Tabs.Screen name='index' options={{
+          tabBarIcon: ({size, color}) => <Ionicons name='home' size={size} color={color}/>
+        }}/>
+        <Tabs.Screen name='bookmark' options={{
+          tabBarIcon: ({size, color}) => <Ionicons name='bookmark' size={size} color={color}/>
+        }}/>
+        <Tabs.Screen name='create' options={{
+          tabBarIcon: ({size, color}) => <Ionicons name='add-circle' size={size} color={COLORS.primary}/>
+        }}/>
+        <Tabs.Screen name='notifications'options={{
+          tabBarIcon: ({size, color}) => <Ionicons name='heart' size={size} color={color}/>
+        }}/>
+        <Tabs.Screen name='profile' options={{
+          tabBarIcon: ({size, color}) => <Ionicons name='person' size={size} color={color}/>
+        }}/>
+    </Tabs>
+  )
+}
---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import { styles } from '../styles/auth.styles'
+import { Link, Redirect } from "expo-router";
+export default function Index() {
+  return <Redirect href={'/(tabs)'}/>
+}
